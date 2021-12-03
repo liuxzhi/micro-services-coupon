fe0f24228cf1511935edf1b4e9f63bfe7a8a9b1b
liuxzhi <15864375581@163.com>
2021-12-03 13:11:24 +0800

    远程商品服务

--- app/Service/CouponGoodsService.php
+++ app/Service/CouponGoodsService.php
@@ -4,12 +4,65 @@ declare(strict_types=1);
 
 namespace App\Service;
 use App\Contract\CouponGoodsServiceInterface;
+use App\Model\CouponGoods;
 use App\Model\Model;
 
 class CouponGoodsService extends AbstractService implements CouponGoodsServiceInterface
 {
-	public function getModelObject(): Model
+
+	/**
+	 * @return Model|mixed
+	 */
+	public function getModelObject() :Model
 	{
-		// TODO: Implement getModelObject() method
+		return make(CouponGoods::class);
+	}
+
+
+	/**
+	 * 删除优惠券和商品的绑定关系
+	 * @param $couponId
+	 *
+	 * @return mixed
+	 */
+	public function deleteCouponGoods($couponId)
+	{
+		return CouponGoods::where("coupon_id", "=", $couponId)->delete();
+	}
+
+	/**
+	 * 创建商品和优惠券的绑定关系
+	 *
+	 * @param $couponId
+	 * @param $params
+	 *
+	 * @return mixed
+	 */
+	public function createCouponGoods($couponId, $params)
+	{
+		$couponGoodsList = $params['scope_goods_ids'];
+		foreach ($couponGoodsList as &$couponGoods) {
+			$couponGoods['coupon_id'] = $couponId;
+			$couponGoods['created_at'] = time();
+			$couponGoods['updated_at'] = time();
+		}
+
+		return CouponGoods::insert($couponGoodsList);
+	}
+
+
+	/**
+	 * 获取优惠券商品列表
+	 * @param array $conditions
+	 * @param array $options
+	 * @param array $columns
+	 * @return array
+	 */
+	public function getCouponGoodsList(array $conditions=[], array $options=[], array $columns = ['*']) :array
+	{
+		$model = $this->getModelObject();
+		$data = $this->optionWhere($model, $conditions, $options)->select($columns)->get();
+		$data || $data = collect([]);
+		return $data->toArray();
 	}
 }
--- app/Service/CouponService.php
+++ app/Service/CouponService.php
@@ -3,13 +3,122 @@
 declare(strict_types=1);
 
 namespace App\Service;
+use App\Constants\Coupon\CouponConstants;
 use App\Contract\CouponServiceInterface;
+use App\Helper\Log;
+use App\Model\Coupon;
+use App\Model\CouponGoods;
 use App\Model\Model;
 
 class CouponService extends AbstractService implements CouponServiceInterface
 {
-	public function getModelObject(): Model
+
+	/**
+	 * 根据查询条件获取属性值
+	 *
+	 * @param array $conditions
+	 * @param array $options
+	 * @param array $columns
+	 *
+	 * @return array
+	 */
+	public function getCouponList(array $conditions = [], array $options = [], array $columns = ['*']): array
 	{
-		// TODO: Implement getModelObject() method.
+		$model = $this->getModelObject();
+		$data = $this->optionWhere($model, $conditions, $options)->select($columns)->get();
+		$data || $data = collect([]);
+		return $data->toArray();
+	}
+
+
+
+	/**
+	 * 获取优惠券商品
+	 *
+	 * @param $couponId
+	 *
+	 * @return array
+	 */
+	public function getCouponGoodsList($couponId)
+	{
+		$data = CouponGoods::where(['coupon_id' => $couponId, "is_delete" => 0])->select(['id', 'app_id', 'coupon_id', 'goods_id'])->get();
+		$data || $data = collect([]);
+
+		return $data->toArray();
+	}
+
+	/**
+	 * 优惠券自动启用
+	 * @return array
+	 */
+	public function enableCoupon(): array
+	{
+		$executeInterval = (int)env('COUPON_EXECUTE_INTERVAL');
+		$conditions = [
+			'state' => CouponConstants::STATE_DISABLE,
+			'automatic_state_on' => CouponConstants::AUTOMATIC_STATE_NO,
+			['receive_start_time', "<=", time()],
+			['receive_start_time', ">=", (time() - $executeInterval)]
+		];
+
+		$couponList = $this->getCouponList($conditions);
+		Log::info("enable_coupon_list", $couponList);
+
+		$result = [];
+		if (!$couponList) {
+			return $result;
+		}
+
+		// 乐观锁保证更新
+		foreach ($couponList as $coupon) {
+			$result[$coupon['id']]['result'] = $this->getModelObject()->where($coupon)->update([
+				                                                                                   'state' => CouponConstants::STATE_ENABLE,
+				                                                                                   'automatic_state_on' => CouponConstants::AUTOMATIC_STATE_YES
+			                                                                                   ]);
+		}
+		return $result;
+	}
+
+	/**
+	 * 过期优惠券自动禁用
+	 *
+	 * @return array
+	 */
+	public function disableCoupon(): array
+	{
+
+		$conditions = [
+			'state' => CouponConstants::STATE_ENABLE,
+			'automatic_state_off' => CouponConstants::AUTOMATIC_STATE_NO,
+			['use_end_time', '<=', time()]
+		];
+
+		$couponList = $this->getCouponList($conditions);
+		Log::info("disable_coupon_list", $couponList);
+
+		$result = [];
+		if (!$couponList) {
+			return $result;
+		}
+
+		// 乐观锁保证更新
+		foreach ($couponList as $coupon) {
+			$result[$coupon['id']]['result'] = $this->getModelObject()->where($coupon)->update([
+				                                                                                   'state' => CouponConstants::STATE_DISABLE,
+				                                                                                   'automatic_state_off' => CouponConstants::AUTOMATIC_STATE_YES
+			                                                                                   ]);
+		}
+
+		return $result;
+	}
+
+
+	/**
+	 * @return Coupon|Model|mixed
+	 */
+
+	public function getModelObject() :Model
+	{
+		return make(Coupon::class);
 	}
 }
