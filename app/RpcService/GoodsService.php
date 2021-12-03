88f8ec5c63f71e09303f8df2d905b84c690fd3d4
liuxzhi <15864375581@163.com>
2021-12-03 11:47:56 +0800

    新增Rpc远程服务层

---
+++ app/RpcService/GoodsService.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\RpcService;
+
+use App\Contract\Rpc\GoodsServiceInterface;
+
+class GoodsService extends RpcBaseService implements GoodsServiceInterface
+{
+
+
+}
---
+++ app/RpcService/RpcBaseService.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\RpcService;
+
+use App\Contract\Rpc\RpcBaseServiceInterface;
+
+class RpcBaseService implements RpcBaseServiceInterface
+{
+
+}
